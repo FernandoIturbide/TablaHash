ebecc756b563b3738e3d3c86c8c3d801de8ae607
Fernando Iturbide Amador <123322837+FernandoIturbide@users.noreply.github.com>
2023-11-20 18:38:39 -0600

    Proeycto
    avances

---
+++ ProyectoEDD/src/main/java/org/example/Controlador/TablaHashLibros.java
@@ -0,0 +1,43 @@
+package org.example.Controlador;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.example.Modelo.Libro;
+
+public class TablaHashLibros {
+    private Map<Integer, Libro> tablaHash;
+
+    public TablaHashLibros() {
+        tablaHash = new HashMap<>();
+    }
+
+    public void agregarLibro(Libro libro) {
+        tablaHash.put(libro.getId(), libro);
+    }
+
+    public Libro buscarLibroPorId(int id) {
+        return tablaHash.get(id);
+    }
+
+    public void eliminarLibro(int id) {
+        tablaHash.remove(id);
+    }
+
+    public void imprimirTabla() {
+        for (Map.Entry<Integer, Libro> entry : tablaHash.entrySet()) {
+            System.out.println("ID: " + entry.getKey() + ", Libro: " + entry.getValue());
+        }
+    }
+    public List<String> obtenerTitulosLibros() {
+    List<String> titulos = new ArrayList<>();
+    for (Libro libro : tablaHash.values()) {
+        titulos.add(libro.getTitulo());
+    }
+    return titulos;
+}
+
+
+}
